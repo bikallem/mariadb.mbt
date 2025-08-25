e16e585e02a065cd522bd23c41d45b5c7bfb11d8
Bikal Gurung <gbikal@gmail.com>
2025-08-25 13:27:30 +0100

    restructure project: move common ffi to internal/ffi package

--- flake.nix
+++ flake.nix
@@ -28,11 +28,34 @@
           pkgs = nixpkgsFor.${system};
         in
         {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              mariadb
-            ];
-          };
+          default =
+            let
+              homeDir = builtins.getEnv "HOME";
+              includePath = [
+                "${homeDir}/.moon/include"
+                "${pkgs.mariadb-connector-c.dev}/include"
+                (builtins.getEnv "C_INCLUDE_PATH")
+              ];
+              libPath = [
+                "${pkgs.mariadb-connector-c.dev}/lib/mariadb"
+                (builtins.getEnv "LIBRARY_PATH")
+              ];
+              ldPath = [
+                libPath
+                (builtins.getEnv "LD_LIBRARY_PATH")
+              ];
+            in
+            pkgs.mkShell {
+              buildInputs = with pkgs; [
+                mariadb
+                mariadb-connector-c
+              ];
+
+              PROJECT_ROOT = builtins.getEnv "PWD";
+              C_INCLUDE_PATH = includePath;
+              C_LIBRARY_PATH = libPath;
+              LD_LIBRARY_PATH = ldPath;
+            };
         }
       );
     };
---
+++ src/internal/ffi/cstring.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "moonbit.h"
+
+MOONBIT_FFI_EXPORT
+moonbit_string_t
+moonbit_string(char *data)
+{
+    if (data == NULL)
+    {
+        return moonbit_make_string(0, 0);
+    }
+    size_t len = strlen(data);
+    moonbit_string_t mb_string = moonbit_make_string(len, 0);
+    for (int i = 0; i < len; i++)
+    {
+        mb_string[i] = (uint16_t)data[i];
+    }
+    return mb_string;
+}
