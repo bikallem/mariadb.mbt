1c30632ea63bbfc5238e04f564747a81aa2c11f2
Bikal Gurung <gbikal@gmail.com>
2025-10-15 18:18:58 +0100

    fmt

--- src/mariadb.c
+++ src/mariadb.c
@@ -3,25 +3,6 @@
 #include <string.h>
 
 MOONBIT_FFI_EXPORT
-moonbit_bytes_t*
-moonbit_row_column_values(MYSQL_ROW row, uint32_t count)
-{
-  moonbit_bytes_t* values =
-    (moonbit_bytes_t*)moonbit_make_ref_array(count, NULL);
-  for (int i = 0; i < count; i++) {
-    if (row[i] == NULL) {
-      values[i] = moonbit_make_bytes(0, 0);
-      continue;
-    }
-    size_t len = strlen((const char*)row[i]);
-    moonbit_bytes_t mb_bytes = moonbit_make_bytes(len, 0);
-    memcpy(mb_bytes, row[i], len);
-    values[i] = mb_bytes;
-  }
-  return values;
-}
-
-MOONBIT_FFI_EXPORT
 MYSQL*
 moonbit_mariadb_connect_via_tcp_socket(MYSQL* mysql,
                                        const char* host,
@@ -46,4 +27,23 @@ moonbit_mariadb_connect_via_unix_socket(MYSQL* mysql,
 {
   return mysql_real_connect(
     mysql, NULL, user, password, database, 0, unix_socket, client_flag);
+}
+
+MOONBIT_FFI_EXPORT
+moonbit_bytes_t*
+moonbit_row_column_values(MYSQL_ROW row, uint32_t count)
+{
+  moonbit_bytes_t* values =
+    (moonbit_bytes_t*)moonbit_make_ref_array(count, NULL);
+  for (int i = 0; i < count; i++) {
+    if (row[i] == NULL) {
+      values[i] = moonbit_make_bytes(0, 0);
+      continue;
+    }
+    size_t len = strlen((const char*)row[i]);
+    moonbit_bytes_t mb_bytes = moonbit_make_bytes(len, 0);
+    memcpy(mb_bytes, row[i], len);
+    values[i] = mb_bytes;
+  }
+  return values;
 }
