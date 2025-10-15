cfe609eda573d1aaa726679d10afd42d6cf2e959
Bikal Gurung <gbikal@gmail.com>
2025-10-15 21:41:30 +0100

    rename type QueryResultRow to MySqlRow

--- src/mysql_res.c
+++ src/mysql_res.c
@@ -42,22 +42,3 @@ moonbit_mariadb_mysql_fetch_row(moonbit_mariadb_mysql_res_t* res_t)
 {
   return mysql_fetch_row(res_t->res);
 }
-
-MOONBIT_FFI_EXPORT
-moonbit_bytes_t*
-moonbit_row_column_values(MYSQL_ROW row, uint32_t count)
-{
-  moonbit_bytes_t* values =
-    (moonbit_bytes_t*)moonbit_make_ref_array(count, NULL);
-  for (int i = 0; i < count; i++) {
-    if (row[i] == NULL) {
-      values[i] = moonbit_make_bytes(0, 0);
-      continue;
-    }
-    size_t len = strlen((const char*)row[i]);
-    moonbit_bytes_t mb_bytes = moonbit_make_bytes(len, 0);
-    memcpy(mb_bytes, row[i], len);
-    values[i] = mb_bytes;
-  }
-  return values;
-}
