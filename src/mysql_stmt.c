aceb5d4f57732e3067b75a852634962810bc248c
Bikal Gurung <gbikal@gmail.com>
2025-10-17 20:13:17 +0100

    feat: statement param value

--- src/mysql_stmt.c
+++ src/mysql_stmt.c
@@ -11,7 +11,7 @@ moonbit_mariadb_mysql_stmt_t_finalize(void* obj)
     moonbit_mariadb_mysql_stmt_t* self = (moonbit_mariadb_mysql_stmt_t*)obj;
     mysql_stmt_close(self->mysql_stmt);
     for (int i = 0; i < self->mysql_binds_count; i++) {
-        free(self->mysql_binds[i].buffer);
+        moonbit_decref(self->mysql_binds[i].buffer);
         free(self->mysql_binds[i].length);
         free(self->mysql_binds[i].is_null);
         free(self->mysql_binds[i].error);
@@ -130,60 +130,60 @@ moonbit_mariadb_stmt_bind_params(moonbit_mariadb_mysql_stmt_t* mysql_stmt_t,
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_set_param_value(MYSQL_BIND* binds,
-                        int32_t index,
-                        const void* value,
-                        uint32_t length)
+moonbit_mariadb_stmt_set_param_value(moonbit_mariadb_mysql_stmt_t* mysql_stmt_t,
+                                     int32_t index,
+                                     moonbit_bytes_t value,
+                                     uint32_t length)
 {
-    memcpy(binds[index].buffer, value, length);
-    *binds[index].is_null = 0; // Set to not null
-    *binds[index].length = length;
+    mysql_stmt_t->mysql_binds[index].buffer = (void*)value;
+    *mysql_stmt_t->mysql_binds[index].is_null = 0; // Set to not null
+    *mysql_stmt_t->mysql_binds[index].length = length;
 }
 
-MOONBIT_FFI_EXPORT
-moonbit_bytes_t*
-moonbit_stmt_result_column_values(MYSQL_STMT* stmt,
-                                  MYSQL_BIND* binds,
-                                  u_int32_t count)
-{
-    moonbit_bytes_t* values =
-      (moonbit_bytes_t*)moonbit_make_ref_array(count, NULL);
-    for (int i = 0; i < count; i++) {
-        if (*binds[i].is_null) {
-            values[i] = moonbit_make_bytes(0, 0);
-            continue;
-        }
-        unsigned long length = *binds[i].length;
-        moonbit_bytes_t mb_bytes = moonbit_make_bytes(length, 0);
-        binds[i].buffer = mb_bytes;
-        binds[i].buffer_length = length;
-        mysql_stmt_fetch_column(stmt, &binds[i], i, 0);
-        binds[i].buffer = NULL;
-        binds[i].buffer_length = 0;
-        values[i] = mb_bytes;
-    }
-    return values;
-}
+// MOONBIT_FFI_EXPORT
+// moonbit_bytes_t*
+// moonbit_stmt_result_column_values(MYSQL_STMT* stmt,
+//                                   MYSQL_BIND* binds,
+//                                   u_int32_t count)
+// {
+//     moonbit_bytes_t* values =
+//       (moonbit_bytes_t*)moonbit_make_ref_array(count, NULL);
+//     for (int i = 0; i < count; i++) {
+//         if (*binds[i].is_null) {
+//             values[i] = moonbit_make_bytes(0, 0);
+//             continue;
+//         }
+//         unsigned long length = *binds[i].length;
+//         moonbit_bytes_t mb_bytes = moonbit_make_bytes(length, 0);
+//         binds[i].buffer = mb_bytes;
+//         binds[i].buffer_length = length;
+//         mysql_stmt_fetch_column(stmt, &binds[i], i, 0);
+//         binds[i].buffer = NULL;
+//         binds[i].buffer_length = 0;
+//         values[i] = mb_bytes;
+//     }
+//     return values;
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t // Bool
-moonbit_mariadb_mysql_stmt_bind_param(
-  moonbit_mariadb_mysql_stmt_t* mysql_stmt_t)
-{
-    return (int32_t)mysql_stmt_bind_param(mysql_stmt_t->mysql_stmt,
-                                          mysql_stmt_t->mysql_binds);
-}
+// MOONBIT_FFI_EXPORT
+// int32_t // Bool
+// moonbit_mariadb_mysql_stmt_bind_param(
+//   moonbit_mariadb_mysql_stmt_t* mysql_stmt_t)
+// {
+//     return (int32_t)mysql_stmt_bind_param(mysql_stmt_t->mysql_stmt,
+//                                           mysql_stmt_t->mysql_binds);
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t
-moonbit_MYSQL_NO_DATA(void)
-{
-    return MYSQL_NO_DATA;
-}
+// MOONBIT_FFI_EXPORT
+// int32_t
+// moonbit_MYSQL_NO_DATA(void)
+// {
+//     return MYSQL_NO_DATA;
+// }
 
-MOONBIT_FFI_EXPORT
-int32_t
-moonbit_MYSQL_DATA_TRUNCATED(void)
-{
-    return MYSQL_DATA_TRUNCATED;
-}
+// MOONBIT_FFI_EXPORT
+// int32_t
+// moonbit_MYSQL_DATA_TRUNCATED(void)
+// {
+//     return MYSQL_DATA_TRUNCATED;
+// }
